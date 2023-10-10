319cf459e15c6b11b06cfedd989a7b3a22659b85
Buetler Tim <tim.buetler@gmail.com>
2023-10-10 15:49:33 +0200

    using enter in textarea is possible

--- PROJECT-NAME/src/app/chat-bar/chat-bar.component.ts
+++ PROJECT-NAME/src/app/chat-bar/chat-bar.component.ts
@@ -8,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class ChatBarComponent implements OnInit{
   constructor() {}
   myVariable:string = "";
-  
+
+  onEnterPressed(event: Event): void {
+    const keyboardEvent = event as KeyboardEvent; // Cast the event to KeyboardEvent
+    if (keyboardEvent.key === 'Enter') {
+      event.preventDefault(); // Prevent the default behavior (new line in textarea)
+      this.myVariable += '\n'; // Add a new line character to the text
+    }
+  }
+  convertNewlinesToBr(text: string): string {
+    return text.replace(/\n/g, '<br>');
+  }
   ngOnInit() {
     
   }
