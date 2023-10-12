cf79011b93dfdb48fa0181eac5daac0eb176776c
Buetler Tim <tim.buetler@gmail.com>
2023-10-12 14:34:46 +0200

    Story Nr 2.1 Eingabe mit Button
    Character Count for overview
    Fixes #12

--- PROJECT-NAME/src/app/chat-bar/chat-bar.component.ts
+++ PROJECT-NAME/src/app/chat-bar/chat-bar.component.ts
@@ -6,14 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-bar.component.css']
 })
 export class ChatBarComponent implements OnInit{
-  constructor() {}
   myVariable:string = "";
+  characterCount: number = 0;
+
+  clearMyVariable() {
+    this.myVariable = "";
+  }
+
+  countCharacters() {
+    this.characterCount = this.myVariable.length;
+    
+/*     if (this.characterCount === 120) {
+      this.myVariable += '\n';
+    } */
+  }
 
   onEnterPressed(event: Event): void {
     const keyboardEvent = event as KeyboardEvent; // Cast the event to KeyboardEvent
     if (keyboardEvent.key === 'Enter') {
       event.preventDefault(); // Prevent the default behavior (new line in textarea)
       this.myVariable += '\n'; // Add a new line character to the text
+      this.characterCount = 0;
     }
   }
   convertNewlinesToBr(text: string): string {
