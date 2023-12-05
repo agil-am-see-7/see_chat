bab4ecd49f6ef93dce530ef0592fc9ba11def20e
Buetler Tim <tim.buetler@gmail.com>
2023-12-05 14:58:19 +0100

    Story Nr 9.1: Features Einbau Autobeleidiger funktioniert. Nickname setzen dan kann direkt losgefeuert werden Fixes #42

--- PROJECT-NAME/src/app/chat-bar/chat-bar.component.ts
+++ PROJECT-NAME/src/app/chat-bar/chat-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, Output, EventEmitter} from '@angular/core';
+import { ChatAlertService } from '../chat-alert.service';
+import { PersonService } from '../person.service';
 
 @Component({
   selector: 'app-chat-bar',
@@ -9,6 +11,9 @@ export class ChatBarComponent {
   //variables
   message: string = "";
   characterCount: number = 0;
+
+  constructor(public cService: ChatAlertService, public pService: PersonService){
+  }
 
   get chatMessage(): string{
     return this.message;
@@ -26,6 +31,13 @@ export class ChatBarComponent {
   countCharacters() {
     this.characterCount = this.message.length;
   }
+  feuern() {
+    const wortObjekt = {
+      "woerter": ["Hurensohn", "Arschgesicht", "Schnäbikätscher", "Arschkriecher", "Nutte"]
+    };
+    const zufaelligesWort = wortObjekt.woerter[Math.floor(Math.random() * wortObjekt.woerter.length)];
+    this.cService.newCard(this.pService.nickname + ": ","Du " + zufaelligesWort + "!");
+  }
 
   // when Submit Button is clicked, it sends the data to the other components
   onPress(){
@@ -36,11 +48,7 @@ export class ChatBarComponent {
         this.chatMessage = "";
         this.characterCount = 0;
         this.chatMessageChange.emit(this.chatMessage = "");
-      }, 10);
-      
+      }, 10);      
     }
   }
-
-
-
 }
